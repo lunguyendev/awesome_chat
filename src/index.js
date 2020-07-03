ef0828c38d489c5641914e7c9ebc75fbf4358fd9
Long Thanh <thanhlongdn.it@gmail.com>
2020-07-04 02:17:12 +0700

    [1.0.0] Config connect DB

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 // var express = require('express');
 import express from "express";
+import ConnecDB from "./config/connectDB"
+import ContactModdel from "./models/contact.model"
+import dotenv  from "dotenv";
+
+
 let app = express();
+dotenv.config();
+//Connect to Mongo
+ConnecDB();
 
 let hostname = "localhost";
 let port = 3000;
 
-app.get("/hello",(req,res)=>{
-    res.send("Hello");
+app.get("/testdb",async (req,res)=>{
+    try {
+        let item = {
+            userId : "121231",
+            contactId: "14546",
+        };
+        let contact = await ContactModdel.createItem(item);
+        res.send(contact);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-app.listen(port,hostname,()=>{
-    console.log("App running at localhost:3000");
+app.listen(process.env.APP_PORT,process.env.APP_HOST,()=>{
+    console.log(`App running at ${process.env.APP_PORT}:${process.env.APP_PORT}`);
 });
-
